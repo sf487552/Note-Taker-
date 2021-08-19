2db8bf4728d113d876e01d967e6ca415a7e703f0
Steve Fokas <sf487552@gmail.com>
2021-08-19 16:26:37 -0400

    route update

--- Routes/htmlRoutes.js
+++ Routes/htmlRoutes.js
@@ -1,12 +1,12 @@
 const path = require('path');
 const router = require('express').Router();
 
-//Sends notes to the notes.html file
+//Sends notes to notes.html 
 router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'))
 })
 
-//Sends to the homepage if a pathing issue exists
+//Directs you to homepage if a pathing issue exists
 router.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'))
 })
