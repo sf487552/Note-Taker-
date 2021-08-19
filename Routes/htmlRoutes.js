e73c70aa2f638945e83bc1eebafc0f4b9179a036
Steve Fokas <sf487552@gmail.com>
2021-08-19 16:10:40 -0400

    bunch of routing changes

--- Routes/htmlRoutes.js
+++ Routes/htmlRoutes.js
@@ -4,12 +4,12 @@ const router = require('express').Router();
 
 //Sends notes to the notes.html file
 router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'Homework09\Develop\public\notes.html'))
+    res.sendFile(path.join(__dirname, '../public/notes.html'))
 })
 
 //Sends to the homepage if a pathing issue exists
 router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'Homework09\Develop\public\index.html'))
+    res.sendFile(path.join(__dirname, '../public/index.html'))
 })
 
 module.exports = router;
